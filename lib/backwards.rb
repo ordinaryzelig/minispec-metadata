9191069ba0f7fd58c86d91941b4cdbf00c670ab5
Jared Ning <jared@redningja.com>
2015-08-02 08:28:22 -0500

    Remove backwards compatibility with __dir__.
    Caused trouble in JRuby.
    It's only used in test to run the readme.spec anyway.

--- lib/backwards.rb
+++ lib/backwards.rb
@@ -7,12 +7,3 @@ if Minitest::Versions::MAJOR <= 4
     end
   end
 end
-
-# Y U NO Ruby 2.0????
-unless Kernel.respond_to?(:__dir__, false)
-  module Kernel
-    def __dir__
-      File.dirname(File.expand_path(__FILE__))
-    end
-  end
-end
