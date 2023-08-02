9237a92412c7eb99769eeec92448d3dd038a3536
Manuel van Rijn <manuel@manuelvanrijn.nl>
2023-08-02 10:42:49 +0200

    use Minitest instead of MiniTest
    the rename happened in 5.0, but 5.19.0 started to only define the old alias when `ENV["MT_COMPAT"]`
    is defined, which usually is not the case

--- lib/minitest/versions.rb
+++ lib/minitest/versions.rb
@@ -1,9 +1,5 @@
 module Minitest
-
   module Versions
-
-    MAJOR, MINOR, PATCH = Minitest::Unit::VERSION.split('.').map(&:to_i)
-
+    MAJOR, MINOR, PATCH = Minitest::VERSION.split('.').map(&:to_i)
   end
-
 end
